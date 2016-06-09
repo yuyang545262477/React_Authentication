a232018f59a73827b046c94ecdc5d5084184aa3f
David_Yu <yuyang545262477@gmail.com>
2016-06-09 12:01:07 +0800

    create the contact constants

---
+++ src/constants/ContactConstants.js
@@ -0,0 +1,7 @@
+import keyMirror from 'keymirror';
+export default KeyMirror({
+    RECEIVE_CONTACT:null, 
+    RECEIVE_CONTACTS:null, 
+    RECEIVE_CONTACT_ERROR:null, 
+    RECEIVE_CONTACTS_ERROR:null
+});
