12e318d84059874f265be79c6437d2fabce0b1f0
David_Yu <yuyang545262477@gmail.com>
2016-06-09 17:57:32 +0800

    create the contact list item component

---
+++ src/components/ContactListItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {ListGroupItem} from "react-bootstrap";
+import {Link} from "react-router";
+
+class ContactListItem extends React.Component {
+    render() {
+        const {contact} = this.props;
+        return (
+            <ListGroupItem>
+                <Link to={'/contact/${contact.id}'}>
+                    <h4>{contact.name}</h4>
+                </Link>
+            </ListGroupItem>
+        )
+    }
+    
+    
+}
+
+export default ContactListItem;
