e893dd1a5076068976b4e334f97ad1ed632aa1ae
David_Yu <yuyang545262477@gmail.com>
2016-06-09 21:42:31 +0800

    create the contact Detail component

---
+++ src/components/ContactDetail.js
@@ -0,0 +1,54 @@
+import React from "react";
+import ContactActions from "../actions/ContactActions";
+import ContactStore from "../stores/ContactStore";
+
+class ContactDetailComponent extends React.Component {
+    
+    constructor() {
+        super();
+        this.state = {
+            contact: {}
+        };
+        this.onChange = this.onChange.bind(this);
+    }
+    
+    componentWillMount() {
+        ContactStore.addChangeListener(this.onChange);
+    }
+    
+    componentDidMount() {
+        ContactActions.getContact(this.props.params.id);
+    }
+    
+    componentWillUnmount() {
+        ContactStore.removeChangeList(this.onChange);
+    }
+    
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            contact: ContactActions.getContact(nextProps.params.id)
+        });
+    }
+    
+    onChange() {
+        this.setState({
+            contact: ContactStore.getContact(this.params.id)
+        });
+    }
+    
+    render() {
+        let contact;
+        if (this.state.contact) {
+            contact = this.state.contact;
+        }
+        return (
+            <div>
+                <img src={contact.image} alt="images" width="150"/>
+                <h1>{contact.name}</h1>
+                <h3>{contact.email}</h3>
+            </div>
+        )
+    }
+}
+
+export default ContactDetailComponent;
